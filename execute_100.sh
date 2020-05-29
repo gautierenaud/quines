baeb1bf41d0fb8766869b54cd60003971c44b47a
Gautier Renaud <gautierenaud@gmail.com>
2020-05-29 21:46:19 +0200

    Add (un)comment mutation

---
+++ execute_100.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./execute_100.sh quine.py"
+    exit 1
+fi
+
+rm -r out
+mkdir -p out
+
+script_name=$1
+
+for i in {1..100}
+do
+  new_file_name=out/tmp_$i.py
+  python3 $script_name > $new_file_name
+  exit_code=$?
+
+  cat $new_file_name | egrep "#\w|#  " && echo "comment at ${i}"
+
+  if [ $exit_code -ne 0 ]; then
+    echo "Breaking (not executable) at ${i}th iteration"
+    exit 1
+  fi
+
+  script_name=$new_file_name
+done
+
+echo "Finished!"
